9267df27712ab6dc56fb440cc29799391c654251
Muralidharan M <48372139+muralidharan1206@users.noreply.github.com>
2025-08-11 13:11:52 +0530

    Update data.tf

--- module/azure_data_bricks/data.tf
+++ module/azure_data_bricks/data.tf
@@ -1,11 +1,17 @@
 # Data sources to dynamically fetch supported spark version and node types
 
 data "databricks_spark_version" "latest" {
+  count             = try(var.cluster_config.use_dynamic_cluster_settings, false) ? 1 : 0
   latest            = true
   long_term_support = true
 }
 
 data "databricks_node_type" "general_purpose" {
+  count      = try(var.cluster_config.use_dynamic_cluster_settings, false) ? 1 : 0
   local_disk = true
   category   = "General Purpose"
+}
+
+data "databricks_group" "admins" {
+  display_name = "admins"
 }
