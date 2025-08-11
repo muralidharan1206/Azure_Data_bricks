9609d0c8b5c6d13d1832e81abba3f7b9fc242f5b
Muralidharan M <48372139+muralidharan1206@users.noreply.github.com>
2025-08-11 13:13:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "databricks" {
-  source                  = "./module/azure_data_bricks"
-  for_each                = local.databricks
+  for_each = { for k, v in local.databricks : k => v if try(v.enabled, true) }
+  #for_each                = local.databricks != null ? local.databricks : {}
+  source                  = "./module/databricks"
   workspace_config        = try(each.value.workspace_config, {})
   cluster_config          = try(each.value.cluster_config, {})
   admin_users             = try(each.value.admin_users, {})
@@ -8,6 +9,7 @@ module "databricks" {
   access_connectors       = try(each.value.access_connectors, {})
   databricks_vnet_peering = try(each.value.databricks_vnet_peering, {})
   vnet_peerings           = try(each.value.vnet_peerings, {})
+  assign_workspace_admin  = try(each.value.assign_workspace_admin, false)
   providers = {
     databricks = databricks.dev2
   }
